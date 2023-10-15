2feecaa3092fb74806ba5d4c19c33f59a8bcc84d
rsancho64 <rsancho@iesrioarba.es>
2023-10-15 21:03:08 +0200

    .

---
+++ w.c/muldiv.h
@@ -0,0 +1,12 @@
+#ifndef MULDIV_H
+#define MULDIV_H
+
+// importaciones:
+#include "sumres.h"
+#include <stdio.h>
+
+// exportaciones:
+int multiplica(int, int);
+int divide(int, int);
+
+#endif
---
+++ w.c/sumres.h
@@ -0,0 +1,10 @@
+#ifndef SUMRES_H
+#define SUMRES_H
+
+// importaciones: no.
+// exportaciones:
+int suma(int, int);
+int resta(int, int);
+
+#endif
+
